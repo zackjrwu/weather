6c0b11452fef2f832041563a9afb8f8e2f956de0
Zack Wu <holyzack1123@hotmail.com>
2019-03-05 11:29:30 +0800

    根據不同城市標籤變換天氣資料

--- ajax.js
+++ ajax.js
@@ -1,21 +1,24 @@
-var countryId = [2306179, 1118370, 15015370, 31278];
+var countryWeather = {
+    today: '05'
+};
+var countryId = [2306179, 1118370, 15015370, 31278, 2459115, 1132599];
 var days = ['今天', '明天', '後天'];
-
+var Time;
 async function getWeatherAW(woeid) {
     try {
         const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
         const data = await result.json();
 
+        //const Country = data.parent.title;   國家
+        const City = data.title; //  城市
+        Time = (data.time).slice(8, 10);
+        console.log(Time);
+        const Pngaddress = 'https://www.metaweather.com/static/img/weather/'; //  天氣小圖示 sn.svg
+
         for (let j = 0; j <= 2; j++) {
-            //const Country = data.parent.title;   國家
-            const City = data.title; //  城市
+
             const Weather = data.consolidated_weather[j].weather_state_name; //  今天天氣
             const WeatherAbb = data.consolidated_weather[j].weather_state_abbr; //  今天天氣簡寫
-            let Time = data.time;
-            const Pngaddress = 'https://www.metaweather.com/static/img/weather/'; //  天氣小圖示 sn.svg
-
-
-            let time = Time.slice(0, 10);
 
             let newAdd = Pngaddress.concat(WeatherAbb);
             let newAdd2 = newAdd.concat('.svg');
@@ -44,6 +47,28 @@ async function getWeatherAW(woeid) {
 
 
 
-countryId.forEach(function (ele) {
-    getWeatherAW(ele);
-});
+// countryId.forEach(function (ele) {
+//     getWeatherAW(ele);
+// });
+
+//TODO: 判斷 localstorage 有無根據 countryId 建立的物件,沒有直接抓取新的天氣資料
+// $(".custom-select").on('change', function () {
+//     $("#shopping-cart tbody").html("");
+//     var $input = $(this);
+//     var country_id = $input.val();
+//     if (!localStorage.countryWeather[countryId]) {
+
+//     } else if (time) {
+
+//         countryId.forEach(function (id) {
+//             //getWeatherAW(country_id);
+//         });
+//     }
+
+
+// });
+
+// getWeatherAW(2306179);
+
+//TODO: 有 countryId 建立的物件，再判斷今天的日期和 localstorage 日期，不同就要抓取新的天氣資料
+var today = new Date().getDate().toString();
